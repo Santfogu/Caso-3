3374aee06fe69f510e8ec0fd1f642ddd55149df4
Santfogu <79651297+Santfogu@users.noreply.github.com>
2023-04-23 00:53:38 -0500

    Analizar todas las combinaciones

--- src/EvaluadorProteccion.java
+++ src/EvaluadorProteccion.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class EvaluadorProteccion {
@@ -22,37 +27,116 @@ public class EvaluadorProteccion {
     }
 
 
+    private void modoPrueba()
+    {
+        String archivo = "datosPrueba.txt";
+        //Estructura archivo de prueba->H,C,S,T
+        //(H) un algoritmo de generación de código criptográfico de hash
+        //(C) una cadena que representa el código criptográfico de hash de una contraseña
+        //(S) una secuencia de 2 caracteres que representa la sal
+        //(T) número de threads que implementará
+
+        String resultados = "";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] campos = linea.split(",");
+                algHash = campos[0];
+                cadenaCodigo = campos[1];
+                sal = campos[2];
+                numThreads = campos[3];
+                if (numThreads.equals("1"))
+                {
+                    HiloDescifrador hilo = new HiloDescifrador(1, algHash, cadenaCodigo, sal); 
+                    hilo.start();
+                    hilo.join();
+                }
+                else if (numThreads.equals("2"))
+                {
+                    HiloDescifrador hilo1 = new HiloDescifrador(2, algHash, cadenaCodigo, sal);
+                    HiloDescifrador hilo2 = new HiloDescifrador(3, algHash, cadenaCodigo, sal);
+                    hilo1.start();
+                    hilo2.start();
+                    hilo1.join();
+                    hilo2.join();
+                }
+                //Escibir resultados por ciclo
+
+
+            }
+        } catch (IOException | InterruptedException e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
+
+        escribirArchivo("Resultados.txt", resultados);
+
+    }
+
+    private File escribirArchivo(String nombre, String data)
+	{
+		File file = new File(nombre);
+		FileWriter fr = null;
+		try {
+			fr = new FileWriter(file);
+			fr.write(data);
+		} 
+		catch (IOException e) {
+			System.out.println("Ocurrió un error al crear el archivo.");
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				fr.close();
+			} 
+			catch (IOException e) {
+				System.out.println("Ocurrió un error al crear el archivo.");
+				e.printStackTrace();
+			}
+		}
+		return file;
+	}
+	
 
     public static void main(String[] args) throws Exception 
     {
         EvaluadorProteccion evaluador = new EvaluadorProteccion();
 		evaluador.imprimirTitulo();
 		Scanner sc = new Scanner ( System.in );
-        
-		System.out.println( "\nPor favor ingrese el algoritmo (H) de generación de código criptográfico de hash (SHA256 o SHA512): ");
-		String algHash = sc.nextLine();
-        System.out.println( "\nPor favor ingrese la cadena (C) que representa el código criptográfico de hash de una contraseña: ");
-		String cadenaCodigo = sc.nextLine();
-        System.out.println( "\nPor favor ingrese una secuencia de 2 caracteres (S) que representa la sal: ");
-		String sal = sc.nextLine();
-        System.out.println( "\nPor favor ingrese el número de threads que implementará (1 o 2): ");
-		String numThreads = sc.nextLine();
-
-        if (numThreads.equals("1"))
+        System.out.println( "\nPor favor ingrese 1 para ingresaar datos por consola y 2 para correr archivo de prueba:");
+		String modo = sc.nextLine();
+        if (modo.equals("1"))
         {
-            HiloDescifrador hilo = new HiloDescifrador(1, algHash, cadenaCodigo, sal); 
-            hilo.start();
+            System.out.println( "\nPor favor ingrese el algoritmo (H) de generación de código criptográfico de hash (SHA-256 o SHA-512): ");
+            String algHash = sc.nextLine();
+            System.out.println( "\nPor favor ingrese la cadena (C) que representa el código criptográfico de hash de una contraseña: ");
+            String cadenaCodigo = sc.nextLine();
+            System.out.println( "\nPor favor ingrese una secuencia de 2 caracteres (S) que representa la sal: ");
+            String sal = sc.nextLine();
+            System.out.println( "\nPor favor ingrese el número de threads que implementará (1 o 2): ");
+            String numThreads = sc.nextLine();
+
+            if (numThreads.equals("1"))
+            {
+                HiloDescifrador hilo = new HiloDescifrador(1, algHash, cadenaCodigo, sal); 
+                hilo.start();
+                hilo.join();
+            }
+            else if (numThreads.equals("2"))
+            {
+                HiloDescifrador hilo1 = new HiloDescifrador(2, algHash, cadenaCodigo, sal);
+                HiloDescifrador hilo2 = new HiloDescifrador(3, algHash, cadenaCodigo, sal);
+                hilo1.start();
+                hilo2.start();
+                hilo1.join();
+                hilo2.join();
+            }
         }
-        else if (numThreads.equals("2"))
+        else if (modo.equals("2"))
         {
-            HiloDescifrador hilo1 = new HiloDescifrador(2, algHash, cadenaCodigo, sal);
-            HiloDescifrador hilo2 = new HiloDescifrador(3, algHash, cadenaCodigo, sal);
-            hilo1.start();
-            hilo2.start();
-        }
+            evaluador.modoPrueba();
         
-
-
+        }
 
     }
 }
--- src/HiloDescifrador.java
+++ src/HiloDescifrador.java
@@ -1,4 +1,9 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+
+import javax.sql.rowset.spi.SyncResolver;
 
 public class HiloDescifrador extends Thread
 {
@@ -21,9 +26,35 @@ public class HiloDescifrador extends Thread
 	
 	public void run() 
 	{
+		System.out.println("Comenzó");
 		crearEspacio("abcdefghijklmnopqrstuvwxyz");
+		long iterador = 0;
+		String V = "";
+		boolean encontrado = false;
 		if (modo == 1)
 		{
+			while (iterador <= 10460353203L && !(encontrado))
+			{
+				int pos1 = (int) (iterador/(27*27*27*27*27*27))%27;
+				int pos2 = (int) (iterador/(27*27*27*27*27))%27;
+				int pos3 = (int) (iterador/(27*27*27*27))%27;
+				int pos4 = (int) (iterador/(27*27*27))%27;
+				int pos5 = (int) (iterador/(27*27))%27;
+				int pos6 = (int) (iterador/27)%27;
+				int pos7 = (int) (iterador%27);
+				
+				V = espacio.get(pos1)+espacio.get(pos2)+espacio.get(pos3)+espacio.get(pos4)+espacio.get(pos5)+espacio.get(pos6)+espacio.get(pos7);
+				System.out.println(V);
+				if (cadenaCodigo.equals(convertirHash(V+sal)))
+				{
+					encontrado = true;
+					System.out.println(V+"---"+sal);
+				}
+
+				iterador++;
+				
+			}
+
 			
 			
 		} 
@@ -35,16 +66,45 @@ public class HiloDescifrador extends Thread
 		{
 			
 		}
+
 		
+		System.out.println("Terminó");
 	}
+
+	private String convertirHash(String cadena)
+	{
+		String hashStr = "";
+		try 
+		{	
+			// Aplicar Hash a los bytes
+			MessageDigest digest;
+			digest = MessageDigest.getInstance(algHash);
+			byte[] hashBytes = digest.digest(cadena.getBytes());
+	
+			// Convertir el valor hash a una cadena hexadecimal
+			StringBuilder sb = new StringBuilder();
+			for (byte b : hashBytes) {
+				sb.append(String.format("%02x", b));
+			}
+			hashStr = sb.toString();
+	
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return hashStr;
+	}
+
 
 
 	private void crearEspacio(String cadena)
 	{
 		espacio.add("");
-		for (int i = 0; i < cadena.length(); i++) {
+		for (int i = 0; i < cadena.length(); i++) 
+		{
 			char c = cadena.charAt(i);
 			espacio.add(""+c);
 		}
 	}
+
+	
 }
